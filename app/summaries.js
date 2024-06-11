bef4cd09f2b4f4e472167765127122053fb4581f
Colin Slater <ctslater@uw.edu>
2024-06-11 15:59:31 -0700

    Separate out collection summary loading.

---
+++ app/summaries.js
@@ -0,0 +1,87 @@
+
+
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3"
+
+const fs = require('fs');
+const zlib = require('zlib');
+
+const  { readFile } = require("fs/promises")
+
+function _GetSummaryS3(repoName, collectionName) {
+
+}
+
+async function GetSummary(repoName, collectionName) {
+
+    if(process.env.BUCKET_NAME) {
+        return _GetSummaryS3(repo, collectionName)
+
+    } else {
+        const gzData = await readFile(`data/${repoName}/collection_${encodeURIComponent(collectionName)}.json.gz`)
+        const collectionData = JSON.parse(zlib.gunzipSync(gzData))
+        return collectionData
+    }
+
+}
+
+async function _ListSummariesS3(repoName) {
+
+    const client = new S3Client({
+        endpoint: process.env.BUCKET_URL,
+        region: "s3dfrgw",
+        forcePathStyle: true,
+        credentials: {
+            accessKeyId: process.env.S3_KEY,
+            secretAccessKey: process.env.S3_SECRET,
+        },
+    })
+    const command = new ListObjectsV2Command({
+        Bucket: process.env.BUCKET_NAME,
+        Prefix: `${repoName}/`
+    });
+
+    try {
+        let isTruncated = true;
+
+        let filenameArrays = []
+
+        while (isTruncated) {
+            const { Contents, IsTruncated, NextContinuationToken } =
+                await client.send(command)
+            filenameArrays.push(Contents.map((entry) => entry.Key))
+            isTruncated = IsTruncated
+            command.input.ContinuationToken = NextContinuationToken
+        }
+        return filenameArrays.flat()
+    } catch (err) {
+        console.log(err)
+        return []
+    }
+
+}
+
+async function _ListSummariesFilesystem(repoName) {
+    const repoDir = `data/${repoName}`
+    try {
+        return fs.readdirSync(repoDir).filter(
+            (filename) => filename.match("collection_(.*).json.gz"));
+    } catch (error) {
+        return []
+    }
+}
+
+async function ListSummaries() {
+
+    const repos = process.env.REPOS.split(',')
+
+    if(process.env.BUCKET_NAME) {
+        const res = await Promise.all(repos.map(repo => _ListSummariesS3(repo)))
+        return res.flat()
+    } else {
+        const res = await Promise.all(repos.map(repo => _ListSummariesFilesystem(repo)))
+        return res.flat()
+    }
+
+}
+
+export {ListSummaries, GetSummary}
