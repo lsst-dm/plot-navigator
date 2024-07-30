f9f45b5534c89b191e74ba75a6388a34501b67bc
Colin Slater <ctslater@uw.edu>
2024-07-30 15:36:08 -0700

    Support redirect to object store URLs.

--- app/image/[repo]/[uuid]/route.js
+++ app/image/[repo]/[uuid]/route.js
@@ -10,16 +10,16 @@ export async function GET(request, { params }) {
     const uuid = params.uuid
     const repoUrl = GetButlerURL(repo)
 
-    const url = `${repoUrl}/v1/get_file/${uuid}`
-    const result = await fetch(url, { next: { revalidate: 10 } })
+    const butlerUrl = `${repoUrl}/v1/get_file/${uuid}`
+    const result = await fetch(butlerUrl, { next: { revalidate: 10 } })
 
     if (!result.ok) {
         try {
             const returnval = await result.json()
-            console.log(`butler get_file failed request url ${url}, result ${JSON.stringify(returnval)}`)
+            console.log(`butler get_file failed request url ${butlerUrl}, result ${JSON.stringify(returnval)}`)
             return new Response("",{status: 500})
         } catch (error) {
-            console.log(`Fetch failed of url ${url}, error ` + error)
+            console.log(`Fetch failed of url ${butlerUrl}, error ` + error)
             return new Response("",{status: 500})
         }
     }
@@ -27,14 +27,16 @@ export async function GET(request, { params }) {
     const datasetInfo = await result.json()
 
     if(datasetInfo.artifact.file_info.length != 1) {
-        console.error(`Multiple file_info entries returned for url ${url}`)
+        console.error(`Multiple file_info entries returned for url ${butlerUrl}`)
         return new Response("",{status: 500})
     }
 
-    if(datasetInfo.artifact.file_info[0].url.startsWith("file")) {
-        const filePath = datasetInfo.artifact.file_info[0].url
+    const fileUrl = datasetInfo.artifact.file_info[0].url
+
+    if(fileUrl.startsWith("file")) {
+
         try {
-            const imgBuffer = fs.readFileSync(filePath.replace("file://",""))
+            const imgBuffer = fs.readFileSync(fileUrl.replace("file://",""))
 
             return new Response(imgBuffer, { headers: { 'content-type': 'image/png' } });
         } catch (error) {
@@ -43,12 +45,16 @@ export async function GET(request, { params }) {
             return new Response("",{status: 500})
 
         }
-    } else {
+    } else if (fileUrl.startsWith("http")) {
         /*
          * This could either redirect to the signed URL, or it could fetch the data and proxy it
          * back to the user.
          */
-        console.error("URL was not type file://, service not implemented")
+        return Response.redirect(fileUrl)
+
+    } else {
+
+        console.error(`URL type not supported, ${fileUrl}`)
         return new Response("",{status: 500})
     }
 
