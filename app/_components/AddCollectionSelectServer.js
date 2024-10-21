a148d704977ce4db20802c8bd8220a5e82ba7af6
Colin Slater <ctslater@uw.edu>
2024-10-20 22:28:55 -0700

    Add front end for caching new collections.

---
+++ app/_components/AddCollectionSelectServer.js
@@ -0,0 +1,25 @@
+
+'use server'
+
+export async function pollJob(jobId) {
+
+    console.log(`pollJob ${jobId}`)
+    let res = await fetch(`http://production-tools/plot-navigator/cache/job/${jobId}`)
+    let data = await res.json()
+    console.log(res.json())
+    return data
+}
+
+export async function putCollection(repo, collectionName) {
+
+    let res = await fetch("http://production-tools/plot-navigator/cache/",
+        {method: "PUT",
+         body: JSON.stringify({repo: repo, collection: collectionName}),
+         headers: {"Content-Type": "application/json"}}
+    )
+    let data = await res.json()
+    console.log(`putCollection ${JSON.stringify(data)}`)
+
+    return data.jobId
+
+}
--- app/page.js
+++ app/page.js
@@ -31,24 +31,34 @@ export default async function Collections() {
 
     return (
         <div>
-            <h1 className="text-2xl m-5">Official Collections</h1>
-            <div className="m-5 border-2 rounded px-2 inline-block my-0" >
-                <table className="divide-y">
-                <thead>
-                    <tr>
-                        <td className={cellClassNames}>Collection</td>
-                        <td className={cellClassNames}>Repo</td>
-                        <td className={`text-right ${cellClassNames}`}>Last Updated</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {officialSummaryRefs.map((summary, n) =>
-                    (<tr key={n}><td className={cellClassNames}><Link href={`/collection/${encodeURIComponent(summary.repo)}/${encodeURIComponent(summary.collection)}`}>{summary.collection}</Link></td>
-                         <td className={`${cellClassNames}`}>{summary.repo}</td>
-                         <td className={`text-right ${cellClassNames}`}>{summary.lastModified.toDateString()}</td>
-                        </tr>))}
-                </tbody>
-                </table>
+            <div className="m-5 inline-block">
+                <div className="flex flex-row items-end">
+                    <div className="">
+                        <h1 className="text-2xl py-4">Official Collections</h1>
+                    </div>
+                    <div className="grow text-right">
+                            <div className="py-4"><Link className="p-2 px-4 m-2 rounded-md text-white bg-sky-600" href="/addcollection">Add Collection</Link></div>
+                    </div>
+                </div>
+                <div className="clear-both"></div>
+                <div className="border-2 rounded px-2 inline-block my-0 " >
+                    <table className="divide-y">
+                    <thead>
+                        <tr>
+                            <td className={cellClassNames}>Collection</td>
+                            <td className={cellClassNames}>Repo</td>
+                            <td className={`text-right ${cellClassNames}`}>Last Updated</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {officialSummaryRefs.map((summary, n) =>
+                        (<tr key={n}><td className={cellClassNames}><Link href={`/collection/${encodeURIComponent(summary.repo)}/${encodeURIComponent(summary.collection)}`}>{summary.collection}</Link></td>
+                             <td className={`${cellClassNames}`}>{summary.repo}</td>
+                             <td className={`text-right ${cellClassNames}`}>{summary.lastModified.toDateString()}</td>
+                            </tr>))}
+                    </tbody>
+                    </table>
+                </div>
             </div>
 
             <h1 className="text-2xl m-5">User Collections</h1>
