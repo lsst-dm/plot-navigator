3af335e771ba7dac3e99f82034201ed0f6b40da9
Colin Slater <ctslater@uw.edu>
2025-08-02 21:46:42 -0700

    Sort collection names in CompareCollectionButton.

--- app/_components/compareCollectionButton.js
+++ app/_components/compareCollectionButton.js
@@ -10,6 +10,13 @@ export default function CompareCollectionButton({baseURL, collectionOptions}) {
     const [selectedOfficial, setSelectedOfficial] = useState("")
     const [selectedUnofficial, setSelectedUnofficial] = useState("")
 
+    const officialOptions = collectionOptions.filter(option => !option.collection.startsWith("u/")).sort((a,b) => (b.lastModified - a.lastModified))
+    const userOptions = collectionOptions.filter(option => option.collection.startsWith("u/")).sort((a,b) => (b.lastModified - a.lastModified))
+
+    const makeOptionString = (option) => {
+        return `${encodeURIComponent(option.repo)}/${encodeURIComponent(option.collection)}`
+    }
+
     return (
         <div>
             <div className="block m-5 mb-0 px-4 py-2 rounded-md text-white bg-sky-600" onClick={() => setShowingPopup(!showingPopup)}>
@@ -17,7 +24,7 @@ export default function CompareCollectionButton({baseURL, collectionOptions}) {
                 Compare Collections <svg className="h-5 w-5 inline" fill="white"><path d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z"></path></svg>
             </span>
             </div>
-            { showingPopup ? 
+            { showingPopup ?
                 <div className="border-2 border-black mx-5 m-2 p-2 absolute z-1 right-0 bg-white">
                     <div className="block">
                         <select className="m-1 p-1" value={selectedOfficial} onChange={(e) => {
@@ -25,9 +32,9 @@ export default function CompareCollectionButton({baseURL, collectionOptions}) {
                         }}>
                             <option value="">Official Collections</option>
                             { 
-                                collectionOptions.filter(option => !option.collection.startsWith("u/")).map(collectionOption => 
-                                    <option key={`${encodeURIComponent(collectionOption.repo)}/${encodeURIComponent(collectionOption.collection)}`}
-                                            value={`${encodeURIComponent(collectionOption.repo)}/${encodeURIComponent(collectionOption.collection)}`}>
+                                officialOptions.map(collectionOption =>
+                                    <option key={makeOptionString(collectionOption)}
+                                            value={makeOptionString(collectionOption)}>
                                         {collectionOption.collection}
                                     </option>
                                 ) 
@@ -41,9 +48,9 @@ export default function CompareCollectionButton({baseURL, collectionOptions}) {
                         }}>
                             <option value="">User Collections</option>
                             { 
-                                collectionOptions.filter(option => option.collection.startsWith("u/")).map(collectionOption =>
-                                    <option key={`${encodeURIComponent(collectionOption.repo)}/${encodeURIComponent(collectionOption.collection)}`}
-                                            value={`${encodeURIComponent(collectionOption.repo)}/${encodeURIComponent(collectionOption.collection)}`}>
+                                userOptions.map(collectionOption =
+                                    <option key={makeOptionString(collectionOption)}
+                                            value={makeOptionString(collectionOption)}>
                                         {collectionOption.collection}
                                     </option>
                                 )
