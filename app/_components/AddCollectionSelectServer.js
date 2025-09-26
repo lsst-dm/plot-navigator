3f2b7554ef907c54007dd51392968fd396b8f972
Colin Slater <ctslater@uw.edu>
2025-09-26 13:59:00 -0700

    Apply automatic formatting to _lib and _components.

--- app/_components/AddCollectionSelectServer.js
+++ app/_components/AddCollectionSelectServer.js
@@ -1,25 +1,23 @@
-
-'use server'
+"use server";
 
 export async function pollJob(jobId) {
-
-    console.log(`pollJob ${jobId}`)
-    let res = await fetch(`http://production-tools/plot-navigator/cache/job/${jobId}`)
-    let data = await res.json()
-    console.log(res.json())
-    return data
+  console.log(`pollJob ${jobId}`);
+  let res = await fetch(
+    `http://production-tools/plot-navigator/cache/job/${jobId}`,
+  );
+  let data = await res.json();
+  console.log(res.json());
+  return data;
 }
 
 export async function putCollection(repo, collectionName) {
+  let res = await fetch("http://production-tools/plot-navigator/cache/", {
+    method: "PUT",
+    body: JSON.stringify({ repo: repo, collection: collectionName }),
+    headers: { "Content-Type": "application/json" },
+  });
+  let data = await res.json();
+  console.log(`putCollection ${JSON.stringify(data)}`);
 
-    let res = await fetch("http://production-tools/plot-navigator/cache/",
-        {method: "PUT",
-         body: JSON.stringify({repo: repo, collection: collectionName}),
-         headers: {"Content-Type": "application/json"}}
-    )
-    let data = await res.json()
-    console.log(`putCollection ${JSON.stringify(data)}`)
-
-    return data.jobId
-
+  return data.jobId;
 }
--- app/_lib/dataIdFuncs.js
+++ app/_lib/dataIdFuncs.js
@@ -1,38 +1,39 @@
-
 export default function DataIdSortFunc(dataIdA, dataIdB) {
+  const bandsOrder = ["u", "g", "r", "i", "z", "y"];
 
-    const bandsOrder = ['u', 'g', 'r', 'i', 'z', 'y']
-
-    if('tract' in dataIdA && 'tract' in dataIdB) {
-        const tract_comparison = dataIdA.tract - dataIdB.tract
-        if(tract_comparison != 0) {
-            return tract_comparison
-        }
+  if ("tract" in dataIdA && "tract" in dataIdB) {
+    const tract_comparison = dataIdA.tract - dataIdB.tract;
+    if (tract_comparison != 0) {
+      return tract_comparison;
     }
+  }
 
-    if('visit' in dataIdA && 'visit' in dataIdB) {
-        const visit_comparison = dataIdA.visit - dataIdB.visit
-        if(visit_comparison != 0) {
-            return visit_comparison
-        }
+  if ("visit" in dataIdA && "visit" in dataIdB) {
+    const visit_comparison = dataIdA.visit - dataIdB.visit;
+    if (visit_comparison != 0) {
+      return visit_comparison;
     }
+  }
 
-    if('band' in dataIdA && 'band' in dataIdB) {
-        const band_comparison = bandsOrder.indexOf(dataIdA.band) - bandsOrder.indexOf(dataIdB.band)
-        if(band_comparison != 0) {
-            return band_comparison
-        }
+  if ("band" in dataIdA && "band" in dataIdB) {
+    const band_comparison =
+      bandsOrder.indexOf(dataIdA.band) - bandsOrder.indexOf(dataIdB.band);
+    if (band_comparison != 0) {
+      return band_comparison;
     }
+  }
 
-    if('physical_filter' in dataIdA && 'physical_filter' in dataIdB) {
-        const physical_filter_comparison = bandsOrder.indexOf(dataIdA.physical_filter[0]) - bandsOrder.indexOf(dataIdB.physical_filter[0])
-        if(physical_filter_comparison != 0) {
-            return physical_filter_comparison
-        }
+  if ("physical_filter" in dataIdA && "physical_filter" in dataIdB) {
+    const physical_filter_comparison =
+      bandsOrder.indexOf(dataIdA.physical_filter[0]) -
+      bandsOrder.indexOf(dataIdB.physical_filter[0]);
+    if (physical_filter_comparison != 0) {
+      return physical_filter_comparison;
     }
+  }
 
-    /* if none of these dimensions are available */
-    return 0
+  /* if none of these dimensions are available */
+  return 0;
 }
 
-export { DataIdSortFunc }
+export { DataIdSortFunc };
