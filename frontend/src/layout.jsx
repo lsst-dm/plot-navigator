ff970b059303bb67bd9572a1d2b8d220acd810ae
Colin Slater <ctslater@uw.edu>
2026-04-22 11:56:15 -0700

    Remove title bar links temporarily.

--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -20,9 +20,6 @@ export default function RootLayout() {
         <ul className="flex flex-row p-2">
           <li className="p-2 px-3 text-xl"><a href="/plot-navigator">Plots</a></li>
           <li className="p-2 px-3 text-xl"><a href="/plot-navigator/more">More Plots</a></li>
-          {(process.env.PAGE_LINKS ?? "").split(',').map((page) =>
-              <li className="p-2 px-3 text-xl" key={page}><a href={`/plot-navigator/${page.toLowerCase()}`}>{page}</a></li>
-          )}
         </ul>
       </div>
       <Outlet />
